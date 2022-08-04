dfbf6a4599be03bdc3a69645fff6caa603501223
Milind Padia <padiamilind50@gmail.com>
2022-08-04 18:30:27 +0530

    Create nested lists.py
    From a list of students and their grades, the program alphabetically prints the name of student(s) having second lowest grade.

---
+++ nested lists.py
@@ -0,0 +1,30 @@
+students = []
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        temp = []
+        name = input()
+        score = float(input())
+        temp.append(name)
+        temp.append(score)
+        students.append(temp)
+
+# sorting students in ascending order based on grades
+sorted_list = sorted(students, key = lambda x: x[1])
+
+#removing students with lowest grade
+lowest_grade = sorted_list[0][1]
+for i in range(len(sorted_list)):
+    if sorted_list[0][1] == lowest_grade:
+        sorted_list.pop(0)
+
+# second lowest grade
+second_lowest_grade = sorted_list[0][1]
+
+# sorting and printing name of student(s) in alphabetical order having second lowest grade
+if len(sorted_list) == 1:
+    print(sorted_list[0][0])
+else:
+    for i in sorted(sorted_list):
+        if i[1] == second_lowest_grade:
+            print(i[0])
