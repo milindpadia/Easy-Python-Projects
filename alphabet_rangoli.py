6fbb3557f4cf533d48e1a1758cd41af9d1288247
Milind Padia <padiamilind50@gmail.com>
2022-11-28 14:51:54 +0530

    Alphabet rangoli
    Read the initial comments to know the function of this program

--- alphabet_rangoli.py
+++ alphabet_rangoli.py
@@ -1,14 +1,37 @@
-# def print_rangoli(size):
-#     # your code goes here
+# this program prints following pattern depending upon input
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
 
-# if __name__ == '__main__':
-#     n = int(input())
-#     print_rangoli(n)
+def print_rangoli(size):
+    alphabets = ['-', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-size = 5
-alphabets = ['-', 'a', 'b', 'c', 'd', 'e']
-dash = "-"
+    previous_list = []
+    temp_list = []
+    reverse_order = []
+    no_of_dashes = size * 2 - 2
+    for i in range(size, 0, -1):
+        temp_list.insert(0, alphabets[i])
+        previous_list = temp_list
+        the_string = ""
+        the_string += alphabets[0] * no_of_dashes
+        the_string += "-".join(previous_list[::-1] + temp_list[1:])
+        the_string += alphabets[0] * no_of_dashes
+        print(the_string)
+        reverse_order.append(the_string)
+        no_of_dashes -= 2
 
-for i in range(1, size*2):
-    print(dash * ((size * 2) -2) + alphabets[size] + dash * ((size * 2) -2))
-    size -= 1
+    reverse_order = reverse_order[::-1]
+    for j in range(1, len(reverse_order)):
+        print(reverse_order[j])
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
